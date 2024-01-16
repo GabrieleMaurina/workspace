bf48e36916e728ff2b290fe67a61ef64697c9541
GabrieleMaurina <gabrielemaurina95@gmail.com>
2024-01-16 13:44:52 -0700

    Improved chess games analysis.

--- python/analyze_chess_games/analyze_chess_games.py
+++ python/analyze_chess_games/analyze_chess_games.py
@@ -1,40 +1,57 @@
+import csv
 import chess.pgn
 import datetime
 import os.path
 
 
-FILE = 'GabrieleMaurina.pgn'
-FILE_PATH = os.path.join(os.path.dirname(__file__), FILE)
+GAMES_PGNS = 'GabrieleMaurina.pgn'
+GAMES_PGNS_PATH = os.path.join(os.path.dirname(__file__), GAMES_PGNS)
+GAMES_CSV = 'GabrieleMaurina.csv'
+GAMES_CSV_PATH = os.path.join(os.path.dirname(__file__), GAMES_CSV)
 
 
 def load_pgns():
-    with open(FILE_PATH) as pgn:
+    with open(GAMES_PGNS_PATH) as pgns:
         i = 0
-        while game:= chess.pgn.read_game(pgn):
+        while game:= chess.pgn.read_game(pgns):
             i += 1
             yield game
-            if i > 10:
-                break
 
 
 def load_games():
     for pgn in load_pgns():
+        headers = pgn.headers
         game = {}
-        utc_date = pgn.headers['UTCDate']
-        utc_time = pgn.headers['UTCTime']
-        game['timestamp'] = datetime.datetime.strptime(utc_date + '-' + utc_time, '%Y.%m.%d-%H:%M:%S')
+        utc_date = headers['UTCDate']
+        utc_time = headers['UTCTime']
+        game['timestamp'] = datetime.datetime.strptime(utc_date + '-' + utc_time, '%Y.%m.%d-%H:%M:%S').astimezone(datetime.timezone.utc)
         if pgn.headers['White'] == 'gabrielemaurina':
-            game['elo'] = int(pgn.headers['WhiteElo'])
-            game['opponent_elo'] = int(pgn.headers['BlackElo'])
+            game['color'] = 'white'
+            game['elo'] = int(headers['WhiteElo'])
+            game['opponent_elo'] = int(headers['BlackElo'])
+            game['score'] = 1 if headers['Result'] == '1-0' else 0 if headers['Result'] == '0-1' else 0.5
         else:
-            game['elo'] = int(pgn.headers['BlackElo'])
-            game['opponent_elo'] = int(pgn.headers['WhiteElo'])
+            game['color'] = 'black'
+            game['elo'] = int(headers['BlackElo'])
+            game['opponent_elo'] = int(headers['WhiteElo'])
+            game['score'] = 0 if headers['Result'] == '1-0' else 1 if headers['Result'] == '0-1' else 0.5
+        r = headers['Result']
+        game['time_control'] = headers['TimeControl']
         yield game
+
+
+def save_games(games):
+    with open(GAMES_CSV_PATH, 'w', newline='') as out:
+        csv_writer = csv.DictWriter(out, fieldnames=games[0].keys())
+        csv_writer.writeheader()
+        for game in games:
+            game['timestamp'] = game['timestamp'].isoformat()
+            csv_writer.writerow(game)
 
 
 def main():
     games = tuple(load_games())
-    print(games)
+    save_games(games)
 
 
 if __name__ == '__main__':
